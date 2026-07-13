2e055fd85b9176c003160c2fa2d2733e7a2b2b15
Kunal Nimje <59009685+Kunal245@users.noreply.github.com>
2026-07-14 02:29:16 +0530

    Revert "Spring backend"

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,1 @@
-// export const BACKEND_URL = "https://backend.killerkunal45.workers.dev"
-export const BACKEND_URL = "http://localhost:8080"
+export const BACKEND_URL = "https://backend.killerkunal45.workers.dev"
